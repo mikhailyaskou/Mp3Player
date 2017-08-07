1403b2589a540381ac38d1fc54deb5e0d6daf7dc
Mikhail Yaskou <mikhailyaskow@gmail.com>
2017-08-07 20:22:05 +0300

    Initial Commit

---
+++ Mp3Player/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mp3Player
+//
+//  Created by Mikhail Yaskou on 07.08.17.
+//  Copyright © 2017 Mikhail Yaskou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
